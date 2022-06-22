18e57432d1c1795826c41d01086342d43f3b5627
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-06-22 20:30:02 +0900

    미니맵 구현+함정 랜덤 생성+미니맵 버그 수정+함정 버그 수정중

---
+++ unityRPGPRACTICE/Assets/Script/ActiveMethod/InstantiateObject.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    public GameObject Enemy;
+    [SerializeField]
+    GameObject SetChar;
+    public bool EndElement = false;
+
+    
+    public List<GameObject> EnemyList;
+
+    private void Start()
+    {
+        EnemyInst();
+    }
+
+    private void Update()
+    {
+        if(EndElement==true && EnemyList.Count == 0)
+        {
+            Debug.Log("스테이지 클리어");
+            GameManager.Instance.EndState();
+            GameManager.Instance.freshTime();
+            EndElement = false;
+        }
+    }
+
+    public void EnemyInst()
+    {
+        GameManager.Instance.SetMobAmount();
+       for (int i = 0; i<GameManager.Instance.MobAmount; i++)
+        {
+            GameObject EnemyOb = Instantiate(Enemy);
+            EnemyOb.transform.position = transform.position;
+            EnemyList.Add(EnemyOb);
+
+            EnemyOb.GetComponent<EnemyTest>().targetCharacter = SetChar;
+            EnemyOb.GetComponent<EnemyTest>().targetTransform = SetChar.transform;
+            EnemyOb.GetComponent<EnemyTest>().InstOb = gameObject;
+            EnemyOb.GetComponent<EnemyTest>().ID = EnemyList.Count;
+            EnemyOb.transform.position = new Vector3(Random.RandomRange(transform.position.x - 10, transform.position.x + 10), transform.position.y, Random.RandomRange(transform.position.z - 10, transform.position.z + 10));
+
+            GameManager.Instance.EnemyObList.Add(EnemyOb);
+        }
+        EndElement = true;
+    }
+
+    void SetRemoveList(GameObject EnemyOb)
+    {
+        EnemyList.Remove(EnemyOb);
+    }
+}
